9f9c63cc76df9ec60994e52de5b69a1628d9d329
Faisal <saleemfs27@gmail.com>
2021-07-02 14:50:38 +0200

    Video selection implemented

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
     console.log(video)
     const imgUrl = video.snippet.thumbnails.default.url
     return (
-        <li className='list-group-item'>
+        <li
+            onClick={() => {
+                onVideoSelect(video)
+            }}
+            className='list-group-item'
+        >
             <div className='video-list media'>
                 <div className='media-left'>
                     <img className='media-object' src={imgUrl} />
